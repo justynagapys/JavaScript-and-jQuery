291087a910e459aa0da9a2a760419bde6135b47e
Justyna Gapys <justynagapys0310@gmail.com>
2020-05-10 21:11:01 +0200

    commit14-mapaUpdate2

--- mapa.js
+++ mapa.js
@@ -17,4 +17,27 @@ function loadScript(){
 
 window.onload = loadScript;                         //Wywołanie zdarzenia onload.
 
+ var mapOptions={
+    zoom: 14,
+    center: new google.maps.LatLng(40.782710, -73.965310),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    
+    panControl: false,
+    zoomControl: true,
+    zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL,
+        position: google.maps.ControlPosition.TOP_RIGHT
+    },
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        position: google.maps.ControlPosition.TOP_LEFT
+    },
+    scaleControl: true,
+    scaleControlOptions:{
+        position: google.maps.ControlPosition.TOP_CENTER
+    },
+    streetViewControl: false,
+    overviewMapControl: false
+ };
  
